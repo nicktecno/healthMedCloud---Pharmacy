79047d48bdb514bd98bb36b57a32f8255ff2096b
NicolasCouto <nicolas.couto@cazco.digital>
2023-01-04 15:45:23 -0300

    feature: webpack config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@ const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   webpack: (config, options) => {
     const { isServer } = options;
-    config.experiments = { topLevelAwait: true };
+    config.experiments = {
+      topLevelAwait: true,
+      asyncWebAssembly: true,
+      layers: true,
+    };
     config.plugins.push(
       new NextFederationPlugin({
         name: "main",
