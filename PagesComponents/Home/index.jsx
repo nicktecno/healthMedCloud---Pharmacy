f32a2756d6211fe4c85b204699660aaeec64ff0f
NicolasCouto <nicolas.couto@cazco.digital>
2023-03-31 16:31:05 -0300

    update: first commit

---
+++ PagesComponents/Home/index.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <HomeComponent />;
+};
+
+export default Home;
--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,22 @@
 import GlobalStyles from "../styles/globals";
 import { defaultLayout } from "../jover.json";
 
-import { LangProvider } from "../Context/LangContext";
-import { AuthProvider } from "../Context/AuthContext";
-import { CartLengthProvider } from "../Context/CartLengthContext";
-import { ColorThemeProvider } from "../Context/ColorTheme";
-import { LocationProvider } from "../Context/Location";
-import { MenuProvider } from "../Context/Menu";
 import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <LangProvider>
-      <AuthProvider>
-        <CartLengthProvider>
-          <ColorThemeProvider>
-            <LocationProvider>
-              <MenuProvider>
-                <ToastContainer />
-                <GlobalStyles
-                  colors={
-                    process.env.NEXT_PUBLIC_REACT_APP_MMP_STATE === "true"
-                      ? selectedMkt
-                      : defaultLayout
-                  }
-                />
+    <>
+      <ToastContainer />
+      <GlobalStyles
+        colors={
+          process.env.NEXT_PUBLIC_REACT_APP_MMP_STATE === "true"
+            ? selectedMkt
+            : defaultLayout
+        }
+      />
 
-                <Component {...pageProps} />
-              </MenuProvider>
-            </LocationProvider>
-          </ColorThemeProvider>
-        </CartLengthProvider>
-      </AuthProvider>
-    </LangProvider>
+      <Component {...pageProps} />
+    </>
   );
 }
 
