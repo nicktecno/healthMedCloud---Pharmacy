de3c1d00dcadcf72ed97b16754735a42d83b5577
NicolasCouto <nicolas.couto@cazco.digital>
2023-04-07 12:18:17 -0300

    update: adjusted to main microfront nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import GlobalStyles from "../styles/globals";
-
+import jover from "../jover";
 import { ToastContainer } from "react-toastify";
+import NextNProgress from "nextjs-progressbar";
 import defaultLayout from "../jover";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress
+        color={jover[0]["default-contrast-color"]}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <ToastContainer />
       <GlobalStyles colors={defaultLayout} />
 
